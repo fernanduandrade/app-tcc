1cf204913fa606f2858e1da9ce679303245175ac
fernanduandrade <onanduandrade@gmail.com>
2022-08-06 12:05:20 -0300

    add: inicio do projeto

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
     <View style={styles.container}>
@@ -13,6 +12,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    padding: '10px',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
